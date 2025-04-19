43db9eb97ba087d5c89132bd3043df2d91c6ad82
aryankumarsingh <aryankumarsingh8340@gmail.com>
2025-04-19 23:30:39 +0530

    Add .vercel to .gitignore and update routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
